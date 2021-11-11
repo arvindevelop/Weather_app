d80378c43a0b6d708b36555362ab310950fccd11
arvindevelop <aks7631353674@gmail.com>
2021-11-11 12:00:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react'
-// import UseState from './component/Hooks/useState';
-//import UseEffect from './component/Hooks/useEffect';
-//import UseReducer from './component/Hooks/useReducer';
-// import Todo from './component/todoReact/Todo';
-//import Restaurant from './component/Basics/Resturant';
 import Temp from './component/weather/Temp';
 
 const App = () => {
-    //return <Restaurant/>;
-    //return <UseEffect/>;
     return <Temp/>;
 };
 
